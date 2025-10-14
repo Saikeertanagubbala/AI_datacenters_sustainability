e6b28535f1bbbb4a6771dcc9f89d147ca6c149b0
Saikeertanagubbala <gubbala3@msu.edu>
2025-10-14 11:32:44 -0400

    check

--- final_streamlit_code.py
+++ final_streamlit_code.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("My Streamlit App")
 st.write("Hello, welcome to my app!")
 
-tab1, tab2, tab3, tab4 = st.tabs(["Pre-processing steps", "ISA", "EDA", "Documentation"], width=100)
+tab1, tab2, tab3, tab4 = st.tabs(["Pre-processing steps", "ISA", "EDA", "Documentation"], width=500)
 
 
