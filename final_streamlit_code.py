cc0ce8867b6d9c3067e41a7948fe8f992bf4faf6
Saikeertanagubbala <gubbala3@msu.edu>
2025-10-14 11:30:32 -0400

    added tabs

--- final_streamlit_code.py
+++ final_streamlit_code.py
@@ -3,13 +3,5 @@ import streamlit as st
 st.title("My Streamlit App")
 st.write("Hello, welcome to my app!")
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+tab1, tab2, tab3 = st.tabs(["Pre-processing steps", "ISA", "EDA", "Documentation"], width=100)
+
