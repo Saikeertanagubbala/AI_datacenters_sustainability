1d87a3bbac0bce64271af76167b6db95a057aead
Saikeertanagubbala <gubbala3@msu.edu>
2025-10-14 15:05:30 -0400

    new deploy

--- final_streamlit_code.py
+++ final_streamlit_code.py
@@ -1,8 +1,45 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+st.set_page_config(layout="wide")
 
-st.title("My Streamlit App")
-st.write("Hello, welcome to my app!")
 
-tab1, tab2, tab3, tab4 = st.tabs(["Pre-processing steps", "ISA", "EDA", "Documentation"], width=1000)
+st.markdown(
+    """
+    <style>
+    .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
+        font-size: 18px !important;
+        padding: 16px 24px !important;
+        font-weight: 600 !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+st.title(":earth_americas: Data Centers and Non-Sustainability Measures :fire:")
+st.write("Learn more about metrics on data centers and their usage of energy and water, pushing for sustainability moving forward.")
 
+tab1, tab2, tab3, tab4 = st.tabs(["Pre-processing steps", "IDA", "EDA", "Documentation"])
+
+with tab1:
+    st.write("Pre-processing content here")
+
+with tab2:
+    st.write("IDA content here")
+
+with tab3:
+    st.write("EDA content here")
+
+with tab4:
+    st.write("The two datasets were from Kaggle.")
+    url1 = "https://www.kaggle.com/datasets/sudalairajkumar/data-centers-energy-and-water-usage"
+    url2 = "https://www.kaggle.com/datasets/anmolkumar/2023-esg-metrics-dataset"
+    multi = '''Dataset 1 (ESG Metrics): [link](%s)
+Dataset 2 (Data Center Metrics): 
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+    st.markdown(multi)
